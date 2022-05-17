9d73c3bc78c8316d0e1303980fc6759d62fc08e8
Purvish Chandarana <purvish.chandarana9@gmail.com>
2022-05-17 23:01:44 +0530

    Implemented basket API using Redis Cache

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,35 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository:IBasketRepository
+    {
+        public readonly IDistributedCache _cache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            _cache=cache;
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _cache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+        public async Task DeleteBasket(string userName)
+        {
+            await _cache.RemoveAsync(userName);
+        }
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _cache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
